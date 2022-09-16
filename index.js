76af1dffa1489a2495a0292677360f27295c0b40
syadur-wdd <syadur.wdd@gmail.com>
2022-09-16 23:49:57 +0600

    Success fully query data

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const app = express();
-const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.use(cors);
-app.use(express.json());
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aohpjmv.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -19,7 +19,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const serviceCollection = client.db("doctors-portal").collection("service");
+    const serviceCollection = client
+      .db("doctors-portal")
+      .collection("services");
+    const bookingCollection = client
+      .db("doctors-portal")
+      .collection("bookings");
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -28,17 +33,39 @@ async function run() {
       res.send(services);
     });
 
+    app.get("/booking", async (req, res) => {
+      const query = {};
+      const cursor = bookingCollection.find(query);
+      const bookings = await cursor.toArray();
+      res.send(bookings);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
+
     console.log("Database Connected");
   } finally {
-    await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello from Dentist");
+  res.send("Hello From Doctor Uncle t0t00t!");
 });
 
 app.listen(port, () => {
-  console.log(`Example Dentist app listening on port ${port}`);
+  console.log(`Doctors App listening on port ${port}`);
 });
